c6101a0db8f67edf3741469adc161afe9019c018
GuidoSP <stereopt@gmail.com>
2020-10-23 16:58:01 +0100

    Clean Tweets

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import Markov from 'markov-strings';
-import cliProgress from 'cli-progress';
 import db from './lib/db';
 
 import {
@@ -9,14 +8,11 @@ import {
 } from './lib/twitHandler.js';
 
 const WHOIDS = {
-	WORLDWIDE: 1,
-	UK: 23424975,
-	PORTUGAL: 23424925
+	WORLDWIDE: 1, UK: 23424975, PORTUGAL: 23424925
 };
 
 const LANGUAGES = {
-	EN: 'en',
-	PT: 'pt'
+	EN: 'en', PT: 'pt'
 }
 
 const ANALYZE_AMOUNT = 25;
@@ -28,5 +24,35 @@ console.log("[TwitterAI]");
 	console.log(" -> " + trend.name);
 
 	const { statuses } = await getTweetsFromTrend(trend.query, LANGUAGES.PT, ANALYZE_AMOUNT);
-	db.set(trend.name, statuses).write();
+	let tweets = statuses.map((eachTweet) => {
+		return CleanTweet(eachTweet);
+	});
+
+	db.set(trend.name, tweets).write();
 })();
+
+function CleanTweet(eachTweet) {
+	console.log(eachTweet);
+	let tweetToSave = eachTweet.text;
+
+	if(eachTweet.truncated && eachTweet.extended_tweet) {
+        tweetToSave = eachTweet.extended_tweet.full_text;
+	}
+	
+	if(eachTweet.retweeted_status) {
+        tweetToSave = eachTweet.retweeted_status.text;
+        if(eachTweet.retweeted_status.truncated && eachTweet.retweeted_status.extended_tweet) {
+            tweetToSave = eachTweet.retweeted_status.extended_tweet.full_text;
+        }
+	}
+	
+	if(tweetToSave.indexOf('https://t.co/') != -1) {
+        tweetToSave = tweetToSave.split('https://t.co/')[0];
+	}
+	
+	if(tweetToSave.startsWith('@')) {
+        tweetToSave = tweetToSave.split(' ').splice(1).join(' ');
+    }
+
+	return tweetToSave;
+}
