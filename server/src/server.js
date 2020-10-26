64153dd926b8a4b4f61cc129baf76d4e70aa6efb
GuidoSP <stereopt@gmail.com>
2020-10-26 17:52:08 +0000

    Express App Setup II

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+}));
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 1337;
-
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
 });
